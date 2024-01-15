644adaa3a020b2714e0ca0b6f6539d5852019ddb
JurBel <jurijs.belozjorovs@saltpay.co>
2024-01-15 17:07:21 +0200

    leads-map 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,40 @@
-//@ts-ignore
 import { OpenStreetMapProvider } from "leaflet-geosearch";
 import { useEffect, useState } from "react";
-import MarkerComp from "./components/MarkerComp";
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { Map, Marker } from "pigeon-maps";
+import { mockedData } from "./data/mockedData";
 
 function App() {
-  const provider = new OpenStreetMapProvider();
-  const [coordinates, setCoordinates] = useState<number[]>([]);
+  const [data, setData] = useState(mockedData);
+  const [isTitle, setIsTitle] = useState(false);
 
   useEffect(() => {
-    provider
-      .search({ query: "17 Hillside Close, Morden" })
-      .then((res: any) => setCoordinates([res[0].x, res[0].y]))
-      .catch((e: any) => console.log(e));
-  }, [provider]);
+    if (isTitle)
+      setTimeout(() => {
+        setIsTitle(false);
+      }, 5000);
+  }, [isTitle]);
 
-  // console.log(coordinates);
-
-  // return <MarkerComp coordinates={coordinates} />;
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
-    // <MapContainer
-    //   zoom={3}
-    //   scrollWheelZoom={false}
-    //   style={{
-    //     height: "`enter code here`400px",
-    //     backgroundColor: "red",
-    //     marginTop: "80px",
-    //     marginBottom: "90px",
-    //   }}
-    // ></MapContainer>
+    <Map defaultCenter={[51.5072, -0.1276]} defaultZoom={12} height={900}>
+      {data.map((el) => {
+        return (
+          <Marker
+            key={el.companyName}
+            onClick={() => setIsTitle(true)}
+            width={50}
+            color={el.scoreResult !== "COLD LEAD" ? "red" : undefined}
+            anchor={el.coordinates as [number, number]}
+          >
+            {isTitle ? (
+              <span style={{ fontWeight: 900, fontSize: "10px" }}>
+                {el.companyName}
+              </span>
+            ) : undefined}
+          </Marker>
+        );
+      })}
+    </Map>
   );
 }
 
